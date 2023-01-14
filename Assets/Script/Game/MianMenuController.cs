8e246ba6950deb7027e523a14357e218adca6acc
WeiluWang <edrj1120@gmail.com>
2023-01-14 00:13:54 -0500

    version1

---
+++ Assets/Script/Game/MianMenuController.cs
@@ -0,0 +1,64 @@
+using System;
+//using GameFramework.Core.GameFramework.Manager;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class MianMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainScreen;
+        [SerializeField] private GameObject _joinScreen;
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _joinButton;
+
+        [SerializeField] private Button _submitCodeButton;
+        [SerializeField] private TextMeshProUGUI _codeText;
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+            _hostButton.onClick.AddListener(OnHostClicked);
+            _joinButton.onClick.AddListener(OnJoinClicked);
+            _submitCodeButton.onClick.AddListener(OnSubmitCodeClicked);
+        }
+
+        private void OnDisable()
+        {
+            _hostButton.onClick.RemoveListener(OnHostClicked);
+            _joinButton.onClick.RemoveListener(OnJoinClicked);
+            _submitCodeButton.onClick.RemoveListener(OnSubmitCodeClicked);
+        }
+
+        private async void OnHostClicked()
+        {
+            Debug.Log("host clicked");
+            bool succeeded = await GameLobbyManager.Instance.CreateLobby();
+            if (succeeded)
+            {
+                SceneManager.LoadSceneAsync("Lobby");
+            }
+        }
+
+        private void OnJoinClicked()
+        {
+            _mainScreen.SetActive(false);
+            _joinScreen.SetActive(true);
+        }
+        private async void OnSubmitCodeClicked()
+        {
+            string code = _codeText.text;
+            Debug.Log(code);
+            code = code.Substring(0, code.Length - 1);
+
+            bool succeeded = await GameLobbyManager.Instance.JoinLobby(code);
+            if (succeeded)
+            {
+                SceneManager.LoadSceneAsync("Lobby");
+            }
+        }
+
+
+    }
+}
